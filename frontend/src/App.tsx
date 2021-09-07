e7b076469a9e99e4666e410e97f59cbc4fd2c658
Mario Faria <mariofaria9@hotmail.com>
2021-09-06 22:59:40 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá Mundo!</h1>
+    <>
+    <NavBar />
+    <div className="container">
+      <h2 className="text-primary"> MarioJavaDev </h2>
+
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 export default App;
