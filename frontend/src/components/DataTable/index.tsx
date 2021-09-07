014034d418f132a8a20ccb776cad077fbaeb9a7b
Mario Faria <mariofaria9@hotmail.com>
2021-09-07 14:01:16 -0300

    Mudanca nomes na tabela

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -14,52 +14,52 @@ const DataTable = () => {
                 <tbody>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
+                        <td>Anakin Barris</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>22456.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Kal-el Bowel</td>
+                        <td>50</td>
+                        <td>52</td>
+                        <td>52879.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Logan Snider</td>
+                        <td>26</td>
+                        <td>32</td>
+                        <td>32456.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Padmé Silver</td>
+                        <td>40</td>
+                        <td>32</td>
+                        <td>20789.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Karina Harrys</td>
+                        <td>31</td>
+                        <td>20</td>
+                        <td>13256.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Bob McDowel</td>
+                        <td>20</td>
+                        <td>19</td>
+                        <td>12450.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Selena Jackart</td>
+                        <td>28</td>
+                        <td>27</td>
+                        <td>11000.00</td>
                     </tr>
                 </tbody>
             </table>
