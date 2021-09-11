323f2d6778e6028b3910ab14f829d1361f69b4ac
Mario Faria <mariofaria9@hotmail.com>
2021-09-10 22:47:05 -0300

    Routes

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,6 @@
 const DataTable = () => {
     return (
+
         <div className="table-responsive">
             <table className="table table-striped table-sm">
                 <thead>
@@ -14,57 +15,17 @@ const DataTable = () => {
                 <tbody>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Anakin Barris</td>
+                        <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>22456.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Kal-el Bowel</td>
-                        <td>50</td>
-                        <td>52</td>
-                        <td>52879.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Logan Snider</td>
-                        <td>26</td>
-                        <td>32</td>
-                        <td>32456.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Padmé Silver</td>
-                        <td>40</td>
-                        <td>32</td>
-                        <td>20789.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Karina Harrys</td>
-                        <td>31</td>
-                        <td>20</td>
-                        <td>13256.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Bob McDowel</td>
-                        <td>20</td>
-                        <td>19</td>
-                        <td>12450.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Selena Jackart</td>
-                        <td>28</td>
-                        <td>27</td>
-                        <td>11000.00</td>
+                        <td>15017.00</td>
                     </tr>
                 </tbody>
             </table>
         </div>
+
     );
 }
+
 
 export default DataTable;
